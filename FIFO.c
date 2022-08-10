45fa01f6b87b11c8cca77ae21db6bd1aaf164da6
KAZI MEHEDI <kazirabbikmhr@gmail.com>
2022-08-11 00:50:56 +0600

    os - final four

---
+++ FIFO.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[5],b[20],n,p=0,q=0,m=0,h,k,i,q1=1;
+    char f='F';
+
+    printf("Enter the Number of Pages:");
+    scanf("%d",&n);
+    printf("Enter %d Page Numbers:",n);
+    for(i=0;i<n;i++)
+        scanf("%d",&b[i]);
+    for(i=0;i<n;i++)
+    {if(p==0)
+        {
+            if(q>=3)
+                q=0;
+            a[q]=b[i];
+            q++;
+            if(q1<3)
+            {
+                q1=q;
+            }
+        }
+        printf("\n%d",b[i]);
+        printf("\t");
+        for(h=0;h<q1;h++)
+            printf("%d",a[h]);
+        if((p==0)&&(q<=3))
+        {
+            printf("-->%c",f);
+            m++;
+        }
+        p=0;
+        for(k=0;k<q1;k++)
+        {
+            if(b[i+1]==a[k])
+                p=1;
+        }
+    }
+    printf("\nNo of faults:%d",m);
+//It is simple and easy to understand &amp; implement.
+//It is efficiently used for small systems
+//It does not cause more overheads
+//The process effectiveness is low.
+//When we increase the number of frames while using FIFO, we are giving more memory
+//to processes. So, page fault should decrease, but here the page faults are increasing.
+}
---
+++ MVT.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+
+int main()
+{
+    int m=0,m1=0,m2=0,p,count=0,i;
+
+    printf("enter the memory capacity:");
+    scanf("%d",&m);
+    printf("enter the no of processes:");
+    scanf("%d",&p);
+    for(i=0;i<p;i++)
+    {
+        printf("\nenter memory req for process%d: ",i+1);
+        scanf("%d",&m1);
+        count=count+m1;
+        if(m1<=m)
+        {
+            if(count==m)
+                printf("there is no further memory remaining:");
+            printf("the memory allocated for process%d is: %d ",i+1,m); m2=m-m1;
+            printf("\nremaining memory is: %d",m2);
+            m=m2;
+        }
+        else
+        {
+            printf("memory is not allocated for process%d",i+1);
+        }
+    }
+
+    printf("\nexternal fragmentation for this process is:%d",m2);
+
+    return 0;
+    //MVT (Multiprogramming with a Variable number of Tasks) is the memory management
+//technique in which each job gets just the amount of memory it needs.
+//That is, the partitioning of memory is dynamic and changes as jobs enter and leave the
+//system.
+//MVT is a more ``efficient&#39;&#39; user of resources.
+//MFT suffers with the problem of internal fragmentation
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
